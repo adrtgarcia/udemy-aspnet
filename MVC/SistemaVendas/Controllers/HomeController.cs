21de4dcb50b53de5e940ca7f2ca8d2ca774308a8
Amanda Duarte <127355878+adrtgarcia@users.noreply.github.com>
2025-09-30 08:50:00 -0300

    comentários removidos

--- MVC/SistemaVendas/Controllers/HomeController.cs
+++ MVC/SistemaVendas/Controllers/HomeController.cs
@@ -8,60 +8,16 @@ namespace SistemaVendas.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        //private readonly IHttpContextAccessor _httpContextAccessor;
 
-        
-         
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         } 
 
-        
-        /*
-        public HomeController(ILogger<HomeController> logger, IHttpContextAccessor httpContextAccessor)
-        {
-            _logger = logger;
-            _httpContextAccessor = httpContextAccessor;
-        }
-        */
-        
-        
         public IActionResult Index()
         {
             return View();
         } 
-        
-        
-        
-        /*
-        public IActionResult Index()
-        {
-            // DEBUG: Verificar se a sessão está ativa
-            var logado = _httpContextAccessor.HttpContext.Session.GetInt32(Sessao.LOGADO);
-            var nomeUsuario = _httpContextAccessor.HttpContext.Session.GetString(Sessao.NOME_USUARIO);
-            var emailUsuario = _httpContextAccessor.HttpContext.Session.GetString(Sessao.EMAIL_USUARIO);
-
-            System.Console.WriteLine($"=== HOME CONTROLLER ===");
-            System.Console.WriteLine($"Usuario logado: {logado}");
-            System.Console.WriteLine($"Nome do usuario: {nomeUsuario}");
-            System.Console.WriteLine($"Email do usuario: {emailUsuario}");
-
-            // Verificar se o usuário está logado
-            if (logado != 1)
-            {
-                System.Console.WriteLine("Usuario nao logado, redirecionando para Login");
-                return RedirectToAction("Index", "Login");
-            }
-
-            // Passar dados do usuário para a view
-            ViewBag.NomeUsuario = nomeUsuario;
-            ViewBag.EmailUsuario = emailUsuario;
-            ViewBag.UsuarioLogado = true;
-
-            return View();
-        } */
-        
         public IActionResult Privacy()
         {
             return View();
